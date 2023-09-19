c2f9c91013759b660f990c15c1f1fadb5b4c4d4e
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-09-19 10:08:09 +0000

    add alias command to install script

--- src/container-structure-test/install.sh
+++ src/container-structure-test/install.sh
@@ -3,3 +3,7 @@
 curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64
 chmod +x container-structure-test-linux-amd64
 mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
+
+if [ ${ADDALIAS} ]; then
+
+echo alias cst='container-structure-test' >> /etc/bash.bashrc
