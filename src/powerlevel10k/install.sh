3c3aa761beee382bce37ca8e14e1a27c2226bb15
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-09-21 11:14:37 +0000

    try hard coded path

--- src/powerlevel10k/install.sh
+++ src/powerlevel10k/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/vscode/.oh-my-zsh/custom/themes/powerlevel10k
