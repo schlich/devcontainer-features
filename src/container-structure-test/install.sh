0bbb0474a6a6bc9606ff9d096a317d5542645297
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-09-21 05:26:33 +0000

    remove fluff

--- src/container-structure-test/install.sh
+++ src/container-structure-test/install.sh
@@ -3,9 +3,3 @@
 curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64
 chmod +x container-structure-test-linux-amd64
 mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
-
-if [ ${ADDALIAS} ]; then
-
-echo "alias cst='container-structure-test'" >> ~/.bash_aliases
-
-fi
