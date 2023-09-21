39405229969089dec29e87bb7c1dfe9abe5c0b32
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-09-21 11:49:18 +0000

    try without .git suffix

--- src/powerlevel10k/install.sh
+++ src/powerlevel10k/install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/vscode/.oh-my-zsh/custom/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k /home/vscode/.oh-my-zsh/custom/themes/powerlevel10k
+
+chown 
