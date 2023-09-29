4320eaff6074cf94c2ede59e0ca09227ca78e75e
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-09-29 07:33:30 +0000

    fix: wget syntax

--- src/grafana/install.sh
+++ src/grafana/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget -wO- https://dl.grafana.com/enterprise/release/grafana-enterprise-10.1.2.linux-amd64.tar.gz | tar -xz
+wget -qO- https://dl.grafana.com/enterprise/release/grafana-enterprise-10.1.2.linux-amd64.tar.gz | tar -xz
 sudo mv grafana-10.1.2/bin/grafana /usr/local/bin
