8ffa7dac361df1769da644eb0472125564511412
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-09-29 08:20:51 +0000

    use apt-get instead of apt

--- src/grafana/install.sh
+++ src/grafana/install.sh
@@ -2,6 +2,6 @@
 
 wget -qO- https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt update
-sudo apt install -y grafana-enterprise
+sudo apt-get update
+sudo apt-get install -y grafana-enterprise
 sudo update-rc.d grafana-server defaults 95 10
