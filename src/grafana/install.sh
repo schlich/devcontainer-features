012346c8649ccb73b537bade7a9fa91fac75839b
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-09-29 08:16:28 +0000

    fix: add grafana server service in devcontainer install script

--- src/grafana/install.sh
+++ src/grafana/install.sh
@@ -4,3 +4,4 @@ wget -qO- https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/ke
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt update
 sudo apt install -y grafana-enterprise
+sudo update-rc.d grafana-server defaults 95 10
