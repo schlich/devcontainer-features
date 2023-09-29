211da6d94655eadb15b8c8b90bcc479aaa8f8b2f
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-09-29 06:15:40 +0000

    feat: include testing tool with default devcontainer feature option

--- src/openslides/install.sh
+++ src/openslides/install.sh
@@ -3,3 +3,11 @@
 wget https://github.com/OpenSlides/openslides-manage-service/releases/download/latest/openslides --no-clobber
 chmod +x openslides
 sudo mv openslides /usr/local/bin
+
+
+if [ ${INCLUDE_TESTING_TOOL} == "true" ]; then
+    wget https://github.com/OpenSlides/openslides-performance/releases/download/v0.0.2/openslides-performance_0.0.2_Linux_x86_64.tar.gz --no-clobber
+    tar -xvf openslides-performance_0.0.2_Linux_x86_64.tar.gz
+    chmod +x openslides-performance
+    sudo mv openslides-performance /usr/local/bin
+fi
