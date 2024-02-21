c66d5a41985836e8a71bfab5594b96f3d8775233
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-02-21 12:13:27 +0000

    ✨: python devcontainer bases

--- src/rye/install.sh
+++ src/rye/install.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -e
 
-curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/share/rye" bash
+if test -f /usr/local/share/rye/env; then
+  echo "Rye already installed!"
+else
+  if test -f /usr/local/bin/python; then
+    curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/share/rye" RYE_TOOLCHAIN="/usr/local/bin/python" bash
+  else
+    if test -f /usr/local/python/current/bin/python; then
+        curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/share/rye" RYE_TOOLCHAIN="/usr/local/python/current/bin/python" bash
+    else
+        curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/share/rye" bash
+    fi
+  fi
+fi
 
 /usr/local/share/rye/shims/rye config --set-bool behavior.use-uv=${UV}
