c04be3758ec942edda160da89a3d393ca14503bc
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-02-23 13:23:49 +0000

    remove library installs

--- test/rye/test.sh
+++ test/rye/test.sh
@@ -4,10 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-if [ -f /usr/sbin/pacman ]; then
-    pacman -Syu --noconfirm
-    pacman -Sy zsh nushell --noconfirm
-fi
 
 check "env script exists" test -f /usr/local/share/rye/env
 check "rye version on bash" rye --version
