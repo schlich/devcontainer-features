a08da6cd7a8eb264a8ddbc844bd1378b34b441b6
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-02-23 08:59:16 +0000

    👷‍♂️: add tests for arch linux

--- src/cypress/install.sh
+++ src/cypress/install.sh
@@ -1,3 +1,11 @@
 set -e
-apt-get update
-apt-get install -y --fix-missing libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+
+if [ -f /usr/sbin/pacman ]; then
+    pacman -Syu --noconfirm
+    pacman -Sy zsh npm --noconfirm
+fi
+
+if [ -z /etc/debian_version ]; then
+    apt-get update
+    apt-get install -y --fix-missing libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+fi
--- test/rye/duplicate.sh
+++ test/rye/duplicate.sh
@@ -5,6 +5,7 @@ set -e
 source dev-container-features-test-lib
 
 check "env script exists" bash -c "test -f /usr/local/share/rye/env"
-check "rye version" bash -c "rye --version"
+check "rye version on bash" bash -c "rye --version"
+
 
 reportResults
