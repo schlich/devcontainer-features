bac0f267dc9c4b444e7b63ac0691e91b96c09e84
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-02-25 15:11:27 +0000

    add feature: pixz

--- src/pixz/install.sh
+++ src/pixz/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-apt-get install -y libarchive-dev liblzma-dev
+
 wget -qO- https://github.com/vasi/pixz/releases/download/v1.0.7/pixz-1.0.7.tar.gz | tar xz
 cd pixz-1.0.7
 ./configure
---
+++ test/pixz/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "test" > bar
+pixz bar bar.xz
