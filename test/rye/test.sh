609d0697838c355c5c8f5dab287bc576a1aa525f
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-02-28 05:10:30 -0600

    🔨: use lib instead of share folder, fix path issues

--- test/rye/test.sh
+++ test/rye/test.sh
@@ -5,7 +5,7 @@ set -e
 source dev-container-features-test-lib
 
 
-check "env script exists" test -f /usr/local/share/rye/env
+check "env script exists" test -f /usr/local/lib/rye/env
 check "rye version on bash" rye --version
 check "Uses uv" rye --version | grep -q 'uv enabled: true'
 
