b9d1c150b9fcf6ebb5bb1f1a8b8ec79f900bdce0
tschlic <tschlic@collagegroup.com>
2024-03-14 06:20:33 -0500

    revert path bug

--- src/rye/install.sh
+++ src/rye/install.sh
@@ -6,8 +6,8 @@ apt-get install curl
 if test -f /usr/local/lib/rye/env; then
   echo "Rye already installed!"
 else
-  if python3; then
-    curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" RYE_TOOLCHAIN="$(which python3)" bash
+  if python; then
+    curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" RYE_TOOLCHAIN="$(which python)" bash
   else
     curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" bash
   fi
