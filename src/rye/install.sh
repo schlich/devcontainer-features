a6bd267c524c458b9e0b0d56dcc7c1dbc84c28cc
tschlic <tschlic@collagegroup.com>
2024-03-14 03:47:43 -0500

    ♻️

--- src/rye/install.sh
+++ src/rye/install.sh
@@ -4,14 +4,10 @@ set -e
 if test -f /usr/local/lib/rye/env; then
   echo "Rye already installed!"
 else
-  if test -f /usr/local/bin/python; then
-    curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" RYE_TOOLCHAIN="/usr/local/bin/python" bash
+  if python; then
+    curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" RYE_TOOLCHAIN="$(which python)" bash
   else
-    if test -f /usr/local/python/current/bin/python; then
-        curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" RYE_TOOLCHAIN="/usr/local/python/current/bin/python" bash
-    else
-        curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" bash
-    fi
+    curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" bash
   fi
 fi
 
