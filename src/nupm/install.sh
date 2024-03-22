da42f33f45147b80166a0cfb1bd331e1af1f96a9
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-03-22 12:54:45 +0000

    ➖: nupm

--- src/nupm/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-
-git clone https://github.com/nushell/nupm.git /usr/local/share/nupm
--- test/nupm/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e 
-
-
-source dev-container-features-test-lib
-
-check "nupm installed" nu -c "use /usr/local/share/nupm/nupm/; nupm -h"
-
-reportResults
