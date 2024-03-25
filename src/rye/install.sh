14ecdbc1c3c74459ab44a371dbc016e9c293632f
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-03-25 00:24:58 +0000

    revert check for existing python

--- src/rye/install.sh
+++ src/rye/install.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 set -e
 
-apt-get install curl
-
-if rye --version; then
-  echo "$(rye --version)"
+if test -f /usr/local/lib/rye/env; then
+  echo "Rye already installed!"
 else
   if python; then
-    curl -sSf https://rye-up.com/get | RYE_TOOLCHAIN="$(which python)" bash
+    curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" RYE_TOOLCHAIN="$(which python)" bash
   else
-    curl -sSf https://rye-up.com/get | bash
+    curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="/usr/local/lib/rye" bash
   fi
 fi
-
 
 /usr/local/lib/rye/shims/rye config --set-bool behavior.use-uv=${UV}
