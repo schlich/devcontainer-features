5022b6890619504002917f65944007fdac41c491
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-03-30 13:05:41 +0000

    rye: remove pin after upstream bugfix

--- src/rye/install.sh
+++ src/rye/install.sh
@@ -5,9 +5,9 @@ if rye --version; then
   echo "$(rye --version)"
 else
   if python; then
-    curl -sSf https://rye-up.com/get | RYE_TOOLCHAIN="$(which python)" RYE_VERSION=0.30.0 bash
+    curl -sSf https://rye-up.com/get | RYE_TOOLCHAIN="$(which python)"  bash
   else
-    curl -sSf https://rye-up.com/get | RYE_VERSION=0.30.0 bash
+    curl -sSf https://rye-up.com/get | bash
   fi
 fi
 
