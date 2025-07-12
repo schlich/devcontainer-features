79a453426f2570fddfa8721c6e186ed9a47a9227
schlich <ty.schlich@gmail.com>
2025-07-12 02:09:16 +0000

    fix: update package dependencies for ubuntu > 24.04

--- src/cypress/install.sh
+++ src/cypress/install.sh
@@ -5,7 +5,13 @@ if [ -f /usr/sbin/pacman ]; then
     pacman -Sy zsh npm --noconfirm
 fi
 
-if [ -z /etc/debian_version ]; then
-    apt-get update
-    apt-get install -y --fix-missing libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+if [ -f /etc/debian_version ]; then
+    . /etc/os-release
+    if [ "$ID" = "ubuntu" ] && [ "${VERSION_ID%%.*}" -ge 24 ]; then
+        apt-get update
+        apt-get install -y --fix-missing libgtk2.0-0t64 libgtk-3-0t64 libgbm-dev libnotify-dev libnss3 libxss1 libasound2t64 libxtst6 xauth xvfb
+    else
+        apt-get update
+        apt-get install -y --fix-missing libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+    fi
 fi
