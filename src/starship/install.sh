1642e9981a14b5a9d83cca6c4a22ae84a3f9d16c
Ty Schlich <ty.schlich@gmail.com>
2025-09-04 22:02:05 +0000

    feat: add character option for configuration

--- src/starship/install.sh
+++ src/starship/install.sh
@@ -1,3 +1,6 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
+
+mkdir -p /usr/local/share/.config
+echo "character = { $CHARACTER }" >> /usr/local/share/.config/starship.toml
